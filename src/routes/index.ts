6e45ec04c04a757837f3b22d851f2f50bdd05f2d
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-05-27 03:59:36 +0530

    code quality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,9 @@
 import authRoutes from "./routes.auth";
 import userRoutes from "./routes.user";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("nolfajfdjlddwjj");
-});
 router.use(authRoutes);
 router.use(userRoutes);
 
